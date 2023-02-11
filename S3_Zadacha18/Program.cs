b599dd76d6b6f49bb16be359e9be4e05691d6c90
Ranis1612 <Ranis1612@mail.ru>
2023-02-11 20:03:05 +0500

    Семинар 3 добавил все файлы

---
+++ S3_Zadacha18/Program.cs
@@ -0,0 +1,62 @@
+﻿// **Задача 18:**
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+// Console.Clear();
+// Console.Write("Введите число от 1 до 4: ");
+// int a = int.Parse(Console.ReadLine()!);
+
+// if(a == 1)                   //X>0&&Y>0
+// {
+// Console.WriteLine("X от 0 до + бесконечности; Y от 0 до + бесконечности ");
+// }
+// if(a == 2)                  // X<0&&Y>0
+// {
+// Console.WriteLine("X от 0 до - бесконечности; Y от 0 до + бесконечности");
+// }
+// if(a == 3)                  //X<0&&Y<0
+// {
+// Console.WriteLine("X от 0 до - бесконечности; Y от 0 до - бесконечности");
+// }
+// if(a == 4)                  // X>0&&Y<0
+// {
+// Console.WriteLine("X от 0 до + бесконечности; Y от 0 до - бесконечности");
+// }
+// if (a < 0 || a > 4){
+//   Console.WriteLine("Число не корректно");
+// };
+
+
+// Вариант преподавателя
+  //switch case
+Console.Clear();
+Console.Write("Введите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine()!);
+
+switch(quarter)
+{
+  case 1:
+  {
+    Console.Write("x > 0; y > 0 ");
+    int x = 0, y = 0;
+    break;
+  }
+   case 2:
+  {
+    Console.Write("x < 0; y > 0 ");
+    break;
+  }
+   case 3:
+  {
+    Console.Write("x < 0; y < 0 ");
+    break;
+  }
+     case 4:
+  {
+    Console.Write("x > 0; y < 0 ");
+    break;
+  }
+default:
+  {
+    Console.Write("Введена неправильная четверть ");
+    break;
+  }
+}
---
+++ S3_Zadacha22/Program.cs
@@ -0,0 +1,65 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+// Console.Clear();
+// Console.Write("Введи число N = ");
+// int N = int.Parse (Console.ReadLine());
+// int a = 0;
+// for(int i=1;i<=N;i++){
+// Console.Write($"{Math.Pow(a+i,2)} ");
+// }
+
+
+//Вариант преподавателя
+// Console.Clear();
+// Console.Write("Введи число N: ");
+// int N = int.Parse (Console.ReadLine());
+// int i = 1;
+// if(N < 0)
+// {
+//   i = N;
+//   N = -1;    // N = N * -1 
+// }
+
+// for(; i <= N; i++)
+// {
+//   Console.Write($" {Math.Pow(i,2)}");
+// }
+
+// Вариант преподавателя через массив
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+int m = N; // m = -10
+if (N < 0)
+{
+i = N; //i = -10
+m = -N;// m = 10
+N = -1; // N = N * (-1) N = -1
+}
+int[] result = new int[m];
+int j = 0;
+for (; i <= N; i++)
+{
+result[j] = i * i;
+j++;
+}
+for ( j = 0; j < m; j++)
+{
+Console.Write($"{result[j]} ");
+}
+
+// Console.Clear();
+// int number = ReadInt("Введите число: ");
+
+// for (int i = 1; i <= number; i++)
+// {
+// Console.Write($"{i * i}, ");
+// }
+
+// int ReadInt(string message)
+// {
+// Console.Write(message);
+// return Convert.ToInt32(Console.ReadLine());
+// }
